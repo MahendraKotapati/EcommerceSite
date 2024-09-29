950c7257cc9aaf879e271a578e636114a770736a
MahendraKotapati <mahendra.kotapati38@gmail.com>
2024-09-29 18:11:02 -0500

    add requirements.txt file

--- ShoppingCart/settings.py
+++ ShoppingCart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#n_sw^&wj04c=5_$f9ob%5jagsh6zy@6qu6h6w@#(57+%_5)=3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'ShoppingCart.urls'
